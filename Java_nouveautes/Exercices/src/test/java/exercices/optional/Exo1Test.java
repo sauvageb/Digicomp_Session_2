07f1a1ff27d3155ef1dd718ed1834267ad8676a2
Boris Sauvage <sauvageboris.pro@gmail.com>
2024-10-31 09:05:31 +0100

    optional : create tests

--- Java_nouveautes/Exercices/src/test/java/exercices/optional/Exo1Test.java
+++ Java_nouveautes/Exercices/src/test/java/exercices/optional/Exo1Test.java
@@ -1,6 +1,7 @@
 package exercices.optional;
 
 import org.junit.Rule;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.rules.ExpectedException;
 
@@ -14,36 +15,59 @@ class Exo1Test {
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
-    private Exo1 exo1 = new Exo1();
+    private final Exo1 exo1 = new Exo1();
 
+    // Actual Exercices
     @Test
     public void findPhoneNumberByName() {
-        Optional<String> phoneNumber = exo1.findPhoneNumberByName("Jos de Vos");
-
-        assertThat(phoneNumber.get()).isEqualTo("016/161616");
+        String phoneNumber = exo1.findPhoneNumberByName("Jos de Vos");
+        assertThat(phoneNumber).isEqualTo("016/161616");
     }
 
     @Test
     public void findPhoneNumberByName_NotFound() {
-        expectedException.expect(NoSuchElementException.class);
-
-        Optional<String> phoneNumber = exo1.findPhoneNumberByName("Jos de Voss");
-
-        phoneNumber.get();
+        String phoneNumber = exo1.findPhoneNumberByName("Jos de Voss");
+        Assertions.assertNull(phoneNumber);
     }
 
     @Test
     public void findNameByPhoneNumber() {
-        Optional<String> name = exo1.findNameByPhoneNumber("016/161616");
-
-        assertThat(name.get()).isEqualTo("Jos de Vos");
+        String name = exo1.findNameByPhoneNumber("016/161616");
+        assertThat(name).isEqualTo("Jos de Vos");
     }
 
     @Test
     public void findNameByPhoneNumber_NotFound() {
-        expectedException.expect(NoSuchElementException.class);
-
-        Optional<String> phoneNumber = exo1.findPhoneNumberByName("016/161619");
-        phoneNumber.get();
+       String phoneNumber = exo1.findPhoneNumberByName("016/161619");
+        Assertions.assertNull(phoneNumber);
     }
+
+    // Optional Exercices
+    @Test
+    public void findPhoneNumberByNameOptional() {
+        Optional<String> phoneNumber = exo1.findPhoneNumberByNameWithOptional("Jos de Vos");
+        Assertions.assertTrue(phoneNumber.isPresent());
+        assertThat(phoneNumber.get()).isEqualTo("016/161616");
+    }
+
+    @Test
+    public void findPhoneNumberByNameOptional_NotFound() {
+        Optional<String> phoneNumber = exo1.findPhoneNumberByNameWithOptional("Jos de Voss");
+        assertThat(phoneNumber).isEmpty();
+    }
+
+    @Test
+    public void findNameByPhoneNumberOptional() {
+        Optional<String> name = exo1.findNameByPhoneNumberWithOptional("016/161616");
+        assertThat(name.isPresent()).isTrue();
+        assertThat(name.get()).isEqualTo("Jos de Vos");
+    }
+
+    @Test
+    public void findNameByPhoneNumberOptional_NotFound() {
+        Optional<String> phoneNumber = exo1.findPhoneNumberByNameWithOptional("016/161619");
+        assertThat(phoneNumber).isEmpty();
+    }
+
+
 }
---
+++ Java_nouveautes/Exercices/src/test/java/exercices/optional/Exo2Test.java
@@ -0,0 +1,46 @@
+package exercices.optional;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class Exo2Test {
+
+    private final Exo2 exo2 = new Exo2();
+
+    // Exercices
+    @Test
+    public void findPhoneNumberByNameAndPunishIfNothingFound_NameFound() {
+        String phoneNumber = exo2.findPhoneNumberByNameAndPunishIfNothingFound("Jos de Vos");
+        assertThat(phoneNumber).isEqualTo("016/161616");
+    }
+
+    @Test
+    public void findPhoneNumberByNameAndPunishIfNothingFound_NameNotFound() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> exo2.findPhoneNumberByNameAndPunishIfNothingFound("Boris Sauvage"))
+                .withMessage("No phone number found");
+    }
+
+    // Optional version to really check
+    @Test
+    public void findPhoneNumberByNameAndPunishIfNothingFoundWithOptional_NameNotFound() {
+        Optional<String> phoneNumber = exo2.findPhoneNumberByNameAndPunishIfNothingFoundWithOptional("Boris Sauvage");
+        assertThat(phoneNumber).isEmpty();
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> phoneNumber.orElseThrow(() -> new IllegalArgumentException("No phone number found")))
+                .withMessage("No phone number found");
+    }
+
+    @Test
+    public void findPhoneNumberByNameAndPunishIfNothingFoundWithOptional_NameFound() {
+        Optional<String> phoneNumber = exo2.findPhoneNumberByNameAndPunishIfNothingFoundWithOptional("Jos de Vos");
+        assertThat(phoneNumber).isPresent();
+        assertThat(phoneNumber.get()).isEqualTo("016/161616");
+    }
+
+}
